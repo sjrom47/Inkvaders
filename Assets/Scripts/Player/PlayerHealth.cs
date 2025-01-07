160821697056664b5896a49e4efb55935ab53820
Alberto Velasco Rodríguez <avelascorod@gmail.com>
2025-01-07 13:12:13 +0100

    damage overlay

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    private float health;
+    public float maxHealth;
+
+    public Image damageOverlay;
+    public float duration;
+    public float fadeSpeed;
+    private float durationTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, 0);
+        Debug.Log(health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damageOverlay.color.a > 0)
+        {
+            durationTimer += Time.deltaTime;
+            if (durationTimer > 0) 
+            {
+                float tempAlpha = damageOverlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, tempAlpha);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            RestoreHealth(20);
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        durationTimer = 0;
+        damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, 1);
+        Debug.Log(health);
+    }
+
+    public void RestoreHealth(float heal)
+    {
+        health += heal;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        Debug.Log(health);
+    }
+}
