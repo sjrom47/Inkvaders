8c6b3b0f332ac9d6ef14c00a56761cf5a46df3ed
Alberto Velasco Rodríguez <avelascorod@gmail.com>
2025-01-08 11:48:50 +0100

    base IA enemigos
    para probar se puede usar la escena UITests

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private NavMeshAgent agent;
+    public NavMeshAgent Agent { get => agent; }
+
+    [SerializeField]
+    private string currentState;
+    public Path path;
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        agent = GetComponent<NavMeshAgent>();
+        stateMachine.Initialize();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CanSeePlayer();
+        //currentState = stateMachine.activeState.ToString();
+    }
+
+    public bool CanSeePlayer()
+    {
+        // any player?
+        if (player != null)
+        {
+            // player close enough?
+            if (Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.forward;
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                // player in field of view?
+                if (angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView) 
+                {
+                    Ray ray = new Ray(transform.position, targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    // any obstacle between player and enemy?
+                    if (Physics.Raycast(ray, out hitInfo, sightDistance))
+                    {
+                        if (hitInfo.transform.gameObject == player)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
