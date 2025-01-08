a8c6d59b4a2541873d845878caffeacd5eebc8b2
Alberto Velasco Rodríguez <avelascorod@gmail.com>
2025-01-08 16:48:14 +0100

    playerhealth en playercontroller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public enum Direction
@@ -22,6 +23,16 @@ public class PlayerController : BaseController
     bool lastShootingValue;
     Direction direction;
     WeaponHolder weaponHolder;
+    
+    // Health
+    private float health;
+    public float maxHealth;
+
+    // Damage Overlay
+    public Image damageOverlay;
+    public float duration;
+    public float fadeSpeed;
+    private float durationTimer;
 
     // Start is called before the first frame update
     void Awake()
@@ -85,6 +96,20 @@ public class PlayerController : BaseController
     }
 
     // Update is called once per frame
+    public void Update() 
+    {
+        // Damage Overlay
+        if (damageOverlay.color.a > 0)
+        {
+            durationTimer += Time.deltaTime;
+            if (durationTimer > 0)
+            {
+                float tempAlpha = damageOverlay.color.a;
+                tempAlpha -= Time.deltaTime * fadeSpeed;
+                damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, tempAlpha);
+            }
+        }
+    }
     
 
     //void CheckInput()
@@ -114,5 +139,21 @@ public class PlayerController : BaseController
     {
         squidTransformCommand.Undo();
     }
-    
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        durationTimer = 0;
+        damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, 1);
+        //Debug.Log(health);
+    }
+
+    public void RestoreHealth(float heal)
+    {
+        health += heal;
+        health = Mathf.Clamp(health, 0, maxHealth);
+        //Debug.Log(health);
+    }
+
 }
