2803f333b8d1811abbee44e8eb8ad172147c9bd9
Alberto Velasco Rodríguez <avelascorod@gmail.com>
2025-01-11 15:53:14 +0100

    barra de vida

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour
@@ -22,6 +23,12 @@ public class Player : MonoBehaviour
     public float duration;
     public float fadeSpeed;
     private float durationTimer;
+
+    // Health Bar
+    private float chipSpeed = 2f;
+    private float lerpTimer;
+    public Image frontHealthBar;
+    public Image backHealthBar;
 
     public bool IsSquid = false;
     public bool isReloading = false;
@@ -59,6 +66,32 @@ public class Player : MonoBehaviour
                     damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, tempAlpha);
                 }
             }
+            UpdateHealthUI();
+        }
+    }
+
+    public void UpdateHealthUI()
+    {
+        float fillFront = frontHealthBar.fillAmount;
+        float fillBack = backHealthBar.fillAmount;
+        float healthFraction = health / maxHealth;
+        if (fillBack > healthFraction) // we have recieved damage
+        {
+            frontHealthBar.fillAmount = healthFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillBack, healthFraction, percentComplete);
+        }
+        if (fillFront < healthFraction) // we have been healed
+        {
+            backHealthBar.fillAmount = healthFraction;
+            backHealthBar.color = Color.green;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete *= percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillFront, healthFraction, percentComplete);
         }
     }
 
@@ -69,6 +102,7 @@ public class Player : MonoBehaviour
         if (CompareTag("Player"))
         {
             durationTimer = 0;
+            lerpTimer = 0f;
             damageOverlay.color = new Color(damageOverlay.color.r, damageOverlay.color.g, damageOverlay.color.b, 1);
         }
         if (health <= 0)
@@ -83,6 +117,7 @@ public class Player : MonoBehaviour
     {
         health += heal;
         health = Mathf.Clamp(health, 0, maxHealth);
+        lerpTimer = 0;
         //Debug.Log(health);
     }
 
